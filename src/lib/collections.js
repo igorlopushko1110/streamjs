7959d8658e6dbcb55b1dcedbb92eaf97f1af7c91
Mikhail Ryukin <mikhail.ryukin@outlook.com>
2021-05-24 18:15:25 +0300

    collection module get method

--- src/lib/collections.js
+++ src/lib/collections.js
@@ -28,6 +28,37 @@ Collections.prototype = {
   entry: function(collection, itemId, itemData) {
     return new CollectionEntry(this, collection, itemId, itemData);
   },
+  
+  get: function(collection, itemId, callback) {
+    /**
+     * get item from collection
+     * @method get
+     * @memberof Collections.prototype
+     * @param  {string}   collection  collection name
+     * @param  {object}   itemId  id for this entry
+     * @param  {requestCallback} callback Callback to call on completion
+     * @return {Promise} Promise object
+     * @example collection.get("food", "0c7db91c-67f9-11e8-bcd9-fe00a9219401")
+     */
+    var self = this;
+    return this.client
+      .get({
+        url: this.buildURL(collection, itemId),
+        signature: this.token,
+      })
+      .then((response) => {
+        let entry = self.client.collections.entry(
+          response.collection,
+          response.id,
+          response.data,
+        );
+        entry.full = response;
+        if (callback) {
+          callback(entry);
+        }
+        return entry;
+      });
+  },
 
 };
 
