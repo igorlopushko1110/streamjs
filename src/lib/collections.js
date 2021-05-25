a78bd380a81f18aa7e8e407ead04f14a144f563d
Mikhail Ryukin <mikhail.ryukin@outlook.com>
2021-05-25 18:32:27 +0300

    add, update module

--- src/lib/collections.js
+++ src/lib/collections.js
@@ -59,6 +59,84 @@ Collections.prototype = {
         return entry;
       });
   },
+  
+  add: function(collection, itemId, itemData, callback) {
+    /**
+     * Add item to collection
+     * @method add
+     * @memberof Collections.prototype
+     * @param  {string}   collection  collection name
+     * @param  {string}   itemId  entry id
+     * @param  {object}   itemData  ObjectStore data
+     * @param  {requestCallback} callback Callback to call on completion
+     * @return {Promise} Promise object
+     * @example collection.add("food", "cheese101", {"name": "cheese burger","toppings": "cheese"})
+     */
+    var self = this;
+
+    if (itemId === null) {
+      itemId = undefined;
+    }
+    var body = {
+      id: itemId,
+      data: itemData,
+    };
+    return this.client
+      .post({
+        url: this.buildURL(collection),
+        body: body,
+        signature: this.token,
+      })
+      .then((response) => {
+        let entry = self.client.collections.entry(
+          response.collection,
+          response.id,
+          response.data,
+        );
+        entry.full = response;
+        if (callback) {
+          callback(entry);
+        }
+        return entry;
+      });
+  },
+
+  update: function(collection, entryId, data, callback) {
+    /**
+     * Update entry in the collection
+     * @method update
+     * @memberof Collections.prototype
+     * @param  {string}   collection  collection name
+     * @param  {object}   entryId  Collection object id
+     * @param  {object}   data  ObjectStore data
+     * @param  {requestCallback} callback Callback to call on completion
+     * @return {Promise} Promise object
+     * @example store.update("0c7db91c-67f9-11e8-bcd9-fe00a9219401", {"name": "cheese burger","toppings": "cheese"})
+     * @example store.update("food", "cheese101", {"name": "cheese burger","toppings": "cheese"})
+     */
+    var self = this;
+    var body = {
+      data,
+    };
+    return this.client
+      .put({
+        url: this.buildURL(collection, entryId),
+        body: body,
+        signature: this.token,
+      })
+      .then((response) => {
+        let entry = self.client.collections.entry(
+          response.collection,
+          response.id,
+          response.data,
+        );
+        entry.full = response;
+        if (callback) {
+          callback(entry);
+        }
+        return entry;
+      });
+  },
 
 };
 
